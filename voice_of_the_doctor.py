fdf743ca7d5513031f91505e35bab7773d933ffb
Nosheen24 <nosheen2425@gmail.com>
2026-01-28 15:49:31 +0500

    Initial commit - AI Doctor Multimodal LLM App

---
+++ voice_of_the_doctor.py
@@ -0,0 +1,48 @@
+#Step1a : Setup Text to Speech–TTS–model (gTTS )
+import os
+from gtts import gTTS
+
+
+def text_to_speech_with_gtts_old(input_text, output_filepath):
+    language="en"
+
+    audioobj= gTTS(
+        text=input_text,
+        lang=language,
+        slow=False
+    )
+    audioobj.save(output_filepath)
+
+input_text="Hey, I Love you Nosheen"
+output_filepath="gtts_testing.mp3"
+#text_to_speech_with_gtts_old(input_text, output_filepath)
+
+
+import subprocess
+import platform
+
+def text_to_speech_with_gtts(input_text, output_filepath):
+    language="en"
+
+    audioobj= gTTS(
+        text=input_text,
+        lang=language,
+        slow=False
+    )
+    audioobj.save(output_filepath)
+    os_name = platform.system()
+    try:
+        if os_name == "Darwin":  # macOS
+            subprocess.run(['afplay', output_filepath])
+        elif os_name == "Windows":  # Windows
+            os.startfile(output_filepath)
+        elif os_name == "Linux":  # Linux
+            subprocess.run(['aplay', output_filepath])  # Alternative: use 'mpg123' or 'ffplay'
+        else:
+            raise OSError("Unsupported operating system")
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+
+
+input_text="Hi I am Nosheen and this is autoplay testing!"
+text_to_speech_with_gtts(input_text=input_text, output_filepath="gtts_testing_autoplay.mp3")
